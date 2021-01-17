5c25994a1b39ea57b636e70feea7a927b5d19270
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-01-17 19:24:12 +0330

    Add SECRET_KEY

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+# Secret key should be changed in the development
+app.config['SECRET_KEY'] = 'gPo4xFh0KnZsUbf2cOXAC9RxdBA2VSEI'
 
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
 
 @app.route('/logout/')
 def logout():
