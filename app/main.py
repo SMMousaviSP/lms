625fc0e48ae3fb93c01538e514ed852de9cb0588
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-01-19 20:12:19 +0330

    Add basic registeration form

--- app/main.py
+++ app/main.py
@@ -1,20 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
+from . import model
+
 
 app = Flask(__name__)
 # Secret key should be changed in the development
-app.config['SECRET_KEY'] = 'gPo4xFh0KnZsUbf2cOXAC9RxdBA2VSEI'
+app.config["SECRET_KEY"] = "gPo4xFh0KnZsUbf2cOXAC9RxdBA2VSEI"
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('base.html')
+    return render_template("base.html")
 
 
-@app.route('/login/')
+@app.route("/login/")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route('/logout/')
+@app.route("/logout/")
 def logout():
     pass
+
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        Username = request.form.get("Username", "")
+        Password = request.form.get("Password", "")
+        LastName = request.form.get("LastName", "")
+        FirstName = request.form.get("FirstName", "")
+        PhoneNumber = request.form.get("PhoneNumber", "")
+        Email = request.form.get("Email", "")
+        Faculty = request.form.get("Faculty", "")
+        Institution = request.form.get("Institution", "")
+        Address = request.form.get("Address", "")
+        success, message = model.create_user(
+            Username=Username,
+            Password=Password,
+            LastName=LastName,
+            FirstName=FirstName,
+            PhoneNumber=PhoneNumber,
+            Email=Email,
+            Faculty=Faculty,
+            Institution=Institution,
+            Address=Address,
+        )
+        if success:
+            flash(
+                "Your account has been created successfully, you can login now.",
+                "success",
+            )
+            return redirect(url_for("login"))
+        flash(message, 'warning')
+    return render_template("register.html")
---
+++ app/model.py
@@ -0,0 +1,78 @@
+from mysql.connector import connect, Error
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import local_settings
+
+
+def get_conn():
+    conn = connect(
+        host=local_settings.MYSQL_HOST,
+        user=local_settings.MYSQL_USER,
+        password=local_settings.MYSQL_PASSWORD,
+        database=local_settings.MYSQL_DB,
+    )
+    return conn
+
+
+def create_user(
+    Username,
+    Password,
+    is_student="DEFAULT",
+    is_teacher="DEFAULT",
+    is_manager="DEFAULT",
+    is_admin="DEFAULT",
+    LastName="DEFAULT",
+    FirstName="DEFAULT",
+    PhoneNumber="DEFAULT",
+    Email="DEFAULT",
+    Faculty="DEFAULT",
+    Institution="DEFAULT",
+    Address="DEFAULT"
+):
+    """
+    CREATE TABLE Users (
+        ID INT NOT NULL AUTO_INCREMENT,
+        Username VARCHAR(64) NOT NULL UNIQUE,
+        Password VARCHAR(255) NOT NULL,
+        is_student BOOLEAN DEFAULT false,
+        is_teacher BOOLEAN DEFAULT false,
+        is_manager BOOLEAN DEFAULT false,
+        is_admin BOOLEAN DEFAULT false,
+        LastName VARCHAR(255),
+        FirstName VARCHAR(255),
+        PhoneNumber CHAR(13),
+        Email VARCHAR(320),
+        Faculty VARCHAR(255),
+        Institution VARCHAR(255),
+        Address VARCHAR(2048),
+        PRIMARY KEY (ID)
+    );
+
+    """
+    if len(Username) < 4:
+        error_message = "Username should be at least 4 characters long"
+        return (False, error_message)
+    if len(Password) < 6:
+        error_message = "Password should be at least 6 characters long"
+        return (False, error_message)
+    conn = get_conn()
+    cur = conn.cursor()
+    Password = generate_password_hash(Password)
+    sql_str = f"""
+        INSERT INTO Users
+        (Username, Password, is_student, is_teacher, is_manager, is_admin,
+        LastName, FirstName, PhoneNumber, Email, Faculty, Institution, Address)
+        VALUES
+        ('{Username}', '{Password}', {is_student}, {is_teacher},
+        {is_manager},{is_admin}, '{LastName}', '{FirstName}', '{PhoneNumber}',
+        '{Email}', '{Faculty}', '{Institution}', '{Address}')
+    """
+    try:
+        cur.execute(sql_str)
+        conn.commit()
+    except Error as e:
+        cur.close()
+        conn.close()
+        return (False, e)
+    cur.close()
+    conn.close()
+    return (True, f"User with username: {Username} created successfully")
